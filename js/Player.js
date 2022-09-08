6cd943ff389d269f5054ea7b77fc7cd013caa900
Hacker <93960319+ayaan115@users.noreply.github.com>
2022-09-08 20:58:08 +0530

    Update Player.js

--- js/Player.js
+++ js/Player.js
@@ -20,7 +20,16 @@ class Player {
   life() {
     push();
    //write a correct code to show lives as three green rectangles in a strainght line.
-   
+   textSize(20);
+    fill("white");
+    text("Player", 280, 40);
+
+    fill(this.life1);
+    rect(180, 50, 70, 30);
+    fill(this.life2);
+    rect(250, 50, 70, 30);
+    fill(this.life3);
+    rect(320, 50, 70, 30);
     pop();
   }
 
